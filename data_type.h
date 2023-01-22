8d8fbf838376f5596dfff3d81f0e3068d6786449
jasonjdominguez99 <jasonjdominguez99@users.noreply.github.com>
2023-01-22 13:26:55 +0000

    Created abstract base class for data types

---
+++ data_type.h
@@ -0,0 +1,21 @@
+// data_type.h
+//
+// Header file for the data types
+// class
+//
+// Author: Jason Dominguez
+// Date: 2023-01-22
+
+#ifndef DATA_TYPE
+#define DATA_TYPE
+
+namespace python
+{
+    class data_type
+    {
+    public:
+        virtual ~data_type() = 0;
+    };
+}
+
+#endif
